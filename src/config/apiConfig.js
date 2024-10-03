2940adc657f22a54359d08eebf1b8566b45a491b
Ayaan Shaikh <Samx7091@gmail.com>
2024-10-03 22:35:24 +0530

    final blog inti

--- src/config/apiConfig.js
+++ src/config/apiConfig.js
@@ -2,8 +2,8 @@ import axios from "axios"
 const jwt = localStorage.getItem('jwt')
 console.log(jwt)
 
-// export const API_BASE_URL = "https://blog-api-production-3703.up.railway.app"
-export const API_BASE_URL = "http://localhost:5858"
+export const API_BASE_URL = "https://blog-api-production-3703.up.railway.app"
+// export const API_BASE_URL = "http://localhost:5858"
 
 export const api = axios.create({
     baseURL:API_BASE_URL,
